65d415753619563b29cbb6ebcddc60af71ee8f86
all3xandra <sviatosana@gmail.com>
2022-10-08 17:52:30 +0200

    gertaerakKopiatuMockIntTest

---
+++ src/test/java/tests/gertaerakKopiatuMockIntTest.java
@@ -0,0 +1,83 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import java.util.Date;
+
+import javax.xml.crypto.Data;
+
+import org.business_logic.BLFacade;
+import org.business_logic.BLFacadeImplementation;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import dataAccess.DataAccess;
+import domain.Event;
+import domain.Team;
+
+@RunWith(MockitoJUnitRunner.class)
+public class gertaerakKopiatuMockIntTest {
+
+	DataAccess da = Mockito.mock(DataAccess.class);
+	
+	@InjectMocks
+	BLFacade sut = new BLFacadeImplementation(da);
+	
+	@Test
+	/*
+	 * Se supone que en la fecha data ya hay un evento. No se puede copiar.
+	 */
+	public void Test1() {
+		
+		Mockito.doReturn(false).when(da).gertaerakKopiatu(
+				Mockito.any(Event.class), Mockito.any(Date.class));
+		
+		/*
+		 * Se puede observar que la fecha en la que se quiere copiar el evento coincide
+		 * con la fecha del evento dado. 'Mockeando' el método del DA, el resultado
+		 * debe de ser False.
+		 */
+		Date input = new Date(2022, 12, 12);
+		Date evDate = new Date(2022, 12, 12);
+		String description = "Madrid-Sevilla";
+		String teams[] = description.split("-");
+		Team t1 = new Team(teams[0]);
+		Team t2 = new Team(teams[1]);
+		Event ev = new Event(description, evDate, t1, t2);
+		
+		boolean res = sut.gertaerakKopiatu(ev, input);
+		
+		assertFalse(res);
+	}
+
+	@Test
+	/*
+	 * Se supone que en la fecha data ya hay un evento. No se puede copiar.
+	 */
+	public void Test2() {
+		
+		Mockito.doReturn(true).when(da).gertaerakKopiatu(
+				Mockito.any(Event.class), Mockito.any(Date.class));
+		
+		/*
+		 * Se puede observar que la fecha en la que se quiere copiar el evento es distinta
+		 * a la fecha del evento dado. 'Mockeando' el método del DA, el resultado
+		 * debe de ser True.
+		 */
+		Date input = new Date(2022, 12, 25);
+		Date evDate = new Date(2022, 12, 12);
+		String description = "Madrid-Sevilla";
+		String teams[] = description.split("-");
+		Team t1 = new Team(teams[0]);
+		Team t2 = new Team(teams[1]);
+		Event ev = new Event(description, evDate, t1, t2);
+		
+		boolean res = sut.gertaerakKopiatu(ev, input);
+		
+		assertTrue(res);
+	}
+}
