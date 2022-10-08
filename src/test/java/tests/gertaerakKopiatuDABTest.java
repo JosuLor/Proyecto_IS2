fc919d91da100b84d367a898385d5afe708b1770
all3xandra <sviatosana@gmail.com>
2022-10-08 17:00:09 +0200

    White Box solved, now it restores the DB.

---
+++ src/test/java/tests/gertaerakKopiatuDABTest.java
@@ -0,0 +1,19 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import dataAccess.DataAccess;
+
+public class gertaerakKopiatuDABTest {
+
+	static DataAccess sut = new DataAccess();
+	
+	
+	@Test
+	public void Test1() {
+		
+	}
+
+}
